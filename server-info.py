41be1881e99fafc1d673c2115f462701511e47e6
Jocel1 lhommee <jocelyn.lhommee@insa-rennes.fr>
2023-07-21 10:31:47 +0200

    linux 2 modification

--- server-info.py
+++ server-info.py
@@ -8,3 +8,6 @@ print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
 print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
 print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
 print("Memory Percent: {:.2f}%".format(memory.percent))
+
+# Get the memory usage information
+memory = psutil.virtualMemory()
